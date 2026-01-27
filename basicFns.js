db92d76122e761bd4c1df318fa201a7707a4491b
vieerr <olivier_paspuel@hotmail.com>
2026-01-27 12:32:50 -0500

    ci: 🎡 add basic fns & tests

---
+++ basicFns.js
@@ -0,0 +1,13 @@
+// basic Fns to test
+
+// resta
+const resta = (a, b) => a - b;
+
+// multiplica
+const multiplica = (a, b) => a * b;
+
+// divide
+const divide = (a, b) => a / b;
+
+
+module.exports = { resta, multiplica, divide };
---
+++ basicFns.test.js
@@ -0,0 +1,24 @@
+const { resta, multiplica, divide } = require('./basicFns');
+
+describe('resta', () => {
+    test('should subtract two positive numbers', () => {
+        expect(resta(10, 5)).toBe(5);
+    });
+    
+});
+
+describe('multiplica', () => {
+    test('should multiply two positive numbers', () => {
+        expect(multiplica(4, 5)).toBe(20);
+    });
+    
+});
+
+describe('divide', () => {
+    test('should divide two positive numbers', () => {
+        expect(divide(10, 2)).toBe(5);
+    });
+    test('should return Infinity when dividing by zero', () => {
+        expect(divide(5, 0)).toBe(Infinity);
+    });
+});
